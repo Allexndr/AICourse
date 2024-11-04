a4a29fb6fc96d41a794915315ef7cdeab094ae99
aleksandr <sanmemik2000@gmail.com>
2024-11-04 22:19:26 +0500

    I added the Gemini IP and made it so that when entering some text or question through index.html a request was sent and I was given a text response

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCrxXOE4h3nfOHGatKQYCxVH089hwmlDZo"
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 app = Flask(__name__)
+
+def generate_course_content(course_description):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(course_description)
+
+    if response.candidates:
+        # Проверяем наличие текста в response.candidates[0]
+        return response.candidates[0].content.parts[0].text
+    else:
+        return "Ошибка: Ответ не был получен"
 
 @app.route('/')
 def index():
@@ -9,8 +24,8 @@ def index():
 @app.route('/generate-course', methods=['POST'])
 def generate_course():
     course_description = request.form['course_description']
-    # Здесь можно добавить логику для генерации курса на основе course_description
-    return f"Course structure for: {course_description}"
+    course_content = generate_course_content(course_description)
+    return f"Generated Course Content: {course_content}"
 
 if __name__ == '__main__':
     app.run(debug=True)
